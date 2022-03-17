c7ab54e81f3a073aab2802634bf56067db8c8dc1
HikariLan <shaokeyibb@126.com>
2022-03-17 15:25:24 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WordPressPCL;
+using WordPressPCL.Models;
+
+namespace TyporaWordPressImageUploader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Invalid arguments.");
+                Environment.Exit(-1);
+            }
+
+            var link = args[0];
+
+            if (!link.EndsWith("/"))
+            {
+                link += "/";
+            }
+
+            Console.WriteLine($"WordPress link: {link}");
+
+            AuthMethod Method;
+
+            if (!Enum.TryParse(args[1], true, out Method))
+            {
+                Console.WriteLine($"Unknown auth method {args[1]}, only Basic and Bearer allowed.");
+                Environment.Exit(-1);
+            }
+
+            var client = new WordPressClient(link)
+            {
+                AuthMethod = Method
+            };
+
+            string[] images;
+
+            switch (Method)
+            {
+                case AuthMethod.Basic:
+                    HandleBasicAuth(args, ref client, out images);
+                    break;
+                case AuthMethod.Bearer:
+                    HandleBearerAuth(args, ref client, out images);
+                    break;
+                default:
+                    return;
+            }
+
+            Console.WriteLine($"Attempt to upload {images.Length} image(s)...");
+
+            PrintImageSource(CreateImageAsync(client, images)).Wait();
+        }
+
+        private static async IAsyncEnumerable<MediaItem> CreateImageAsync(WordPressClient client, string[] images)
+        {
+            foreach (var image in images)
+            {
+                yield return await client.Media.CreateAsync(image, System.IO.Path.GetFileName(image));
+            }
+        }
+
+        private static async Task PrintImageSource(IAsyncEnumerable<MediaItem> item)
+        {
+            await foreach (var image in item)
+            {
+                Console.WriteLine(image.SourceUrl);
+            }
+        }
+
+        private static void HandleBearerAuth(string[] args, ref WordPressClient client, out string[] images)
+        {
+            if (args.Length < 4)
+            {
+                Console.WriteLine("Invalid arguments.");
+                Environment.Exit(-1);
+            }
+
+            var token = args[2];
+
+            client.Auth.SetJWToken(token);
+
+            Console.WriteLine($"Entered token: {token}");
+
+            if (!client.Auth.IsValidJWTokenAsync().Result)
+            {
+                Console.WriteLine("Invalid token.");
+                Environment.Exit(-999);
+            }
+
+            images = args[3..args.Length];
+        }
+
+        private static void HandleBasicAuth(string[] args, ref WordPressClient client, out string[] images)
+        {
+            if (args.Length < 5)
+            {
+                Console.WriteLine("Invalid arguments.");
+                Environment.Exit(-1);
+            }
+
+            var username = args[2];
+            var password = args[3];
+
+            client.Auth.SetApplicationPassword(password);
+
+            Console.WriteLine($"Entered username: {username}");
+
+            if (!client.Auth.IsValidJWTokenAsync().Result)
+            {
+                Console.WriteLine("Invalid username or password.");
+                Environment.Exit(-999);
+            }
+
+            images = args[4..args.Length];
+        }
+    }
+}
---
+++ README_CN.md
@@ -0,0 +1,70 @@
+# TyporaWordPressImageUploader
+
+[English](/README.md) 简体中文 
+
+TyporaWordPressImageUploader 是一个基于 .Net Core 3.1 的 C# 应用，它允许你通过 Typora 的自定义命令行图片上传器将你的本地图片自动上传到你的 WordPress 媒体中并在 Markdown 代码中以远程链接显示。
+
+## Install and Deploy
+
+### On WordPress
+
+You need to following these steps to enable the JWT authentication for the WordPress Rest API, and so we can upload the images.
+
+1. Install [JWT Authentication for WP REST API](https://wordpress.org/plugins/jwt-authentication-for-wp-rest-api/) to your WordPress
+2. Make sure you have installed JWT Authentication for WP REST API plugin, and configurated it by following the link above. especially finished the CONFIGURATE THE SECRET KEY.
+3. That's all, and now you need to do something in your local machine.
+
+### On Typora
+
+We have two ways for you to authentication, but we recommend first one because the another one may have broken.
+
+#### Authenticate with token
+
+First, send a post request to your WordPress Rest API endpoint like this:
+
+```HTTP
+POST https://{your wordpress domain here}/wp-json/jwt-auth/v1/token
+Content-Type: application/json
+
+{
+  "username": "{your username}",
+  "password": "{your password}"
+}
+```
+
+And you will receive a JSON response contains your token:
+
+```JSON
+{
+  "token": "{your token here}",
+  "user_email": "...",
+  "user_nicename": "...",
+  "user_display_name": "..."
+}
+```
+
+And now, open the Typora, go to `Preference - Image - Image Upload Setting`, select the `Image Uploader` to `Custom Command`, then type these in the `Command` text field:
+
+```Shell
+"path-to-TyporaWordPressImageUploader.exe" "https://{your wordpress domain here}/wp-json" Bearer {your token here}
+```
+
+Next, press the `Test Uploader` button to check the connection is ok. 
+
+And that's all, next when you need to use a local image in the Typora, just drag it to the main window, and press "Upload Image", and then the image will automatically upload to your WordPress!
+
+#### Authenticate with username and password
+
+Authenticate with username and password is very simple, you can just directly go to the same way in Typora like above, and in the `Command` text field type:
+
+```Shell
+"path-to-TyporaWordPressImageUploader.exe" "https://{your wordpress domain here}/wp-json" Basic {your username here} {your password here}
+```
+
+## Libraries Used
+
+- [WordPressPCL](https://github.com/wp-net/WordPressPCL)
+
+## License
+
+TyporaWordPressImageUploader is licensed under the [Apache License 2.0](/LICENSE).
